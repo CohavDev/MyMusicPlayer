ef9f04a0de5bc85af6e2e157e73be5c8d286135a
CohavDev <shaulspam@gmail.com>
2017-12-08 15:03:11 +0200

    secondary activity

--- app/src/main/java/com/cohav/mymusicplayer/searchMusic/DwnFragment.java
+++ app/src/main/java/com/cohav/mymusicplayer/searchMusic/DwnFragment.java
@@ -93,7 +93,7 @@ public class DwnFragment extends Fragment {
             }
             tag = f.getTagAndConvertOrCreateAndSetDefault();
             tag.setField(FieldKey.TITLE, title);
-            tag.setField(FieldKey.ARTIST, author);
+            tag.setField(FieldKey.ARTIST, author);//need to apply in another place
             tag.setField(FieldKey.ALBUM, title);
             Wrapper w = new Wrapper(f, tag, thumbnailUrl);
             //async task
